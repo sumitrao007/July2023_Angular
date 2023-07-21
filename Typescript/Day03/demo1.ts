42aae420f297c9e6121a51a68b5b9a3e39e30ec0
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2023-07-21 20:25:03 +0530

    Day03

---
+++ Typescript/Day03/demo1.ts
@@ -0,0 +1,139 @@
+
+var a1,a2;
+var a_1,a$1;
+
+// data types 
+// number(int,long,short,double,float),string(""/''),boolean (true/false)
+// any => number,string,boolean
+// void => it is neutral data type => it does not return/point anything
+// misec null & undefined => u can use value as well data type 
+// Object,unknown
+
+var a:number=10;
+// console.log("Value of a is "+a);
+
+var str!:string;
+
+// console.log('Value of str is '+str);
+
+var b:any;
+// b=2.5;
+// console.log("Value of b is "+b);
+// b="Sumit";
+// console.log("Value of b is "+b);
+// b=true;
+// console.log("Value of b is "+b);
+
+// literal
+
+var c:number|string;
+c=5;
+c='Sumit';
+// c=true;
+
+var d:50|null;
+d=50;
+// d=100;
+
+
+var t=78;
+var s;
+
+
+//Type assertion
+// it is used to tells the tsc compiler 
+// pre-requiestic 
+// any/object/unknown
+
+var p:any;
+// 1 Angle bracket => used in ts file 
+// 2 as syntax  => used in ts as well as html file 
+
+// 1 Angle bracket
+
+var temp=(<string> p );
+// temp.
+
+// 2 as syntax
+
+var temp1=(p as string);
+
+// temp1.
+
+//operator
+// airthematic => +,-,/,*,%
+// bitwise => &,|,!,~,^,>>,<<
+// logical => &&, ||
+// relational => <,>,<=,>=,!=,==(it checks only value),===(strongly equality it checks value as well as data type )
+// assignment => =,+=,-=,/=,*=,%=
+// unary => post/pre inc/dec ++a,a--
+// ternary => condition?expression1:expression2
+
+
+// control statement/sequential statement
+// if,if-else,nested if-else,switch,break,continue
+
+// var a:number=21;
+// if(a<4){
+//     console.log("Condition is true ");
+
+// }else {
+//     console.log("Condition is false ");
+// }
+
+var choice=21;
+
+// switch(choice){
+//     case 1: console.log("U r in case 1");
+//             break;
+//     case 2: console.log("U r in case 2");
+//             break;
+//     default: console.log("U r in Default case ");
+//             break;          
+
+// }
+
+// loop statement
+// while,do-while,for,foreach
+
+var count=4;
+// while(count!=0){
+//     console.log("Count is "+count);
+//     count--;    
+// }
+
+// do{
+//     console.log("----- Count is "+count);
+//     count--; 
+// }while(count!=0)
+
+// for 
+// for(var i=0;i<4;i++){
+//     console.log("Value of i "+i);
+// }
+
+// console.log("Access i after the loop "+i);
+
+// var => scope it has global scope 
+// let => it is a keyword to define a variable 
+// scope => within nearest block ({})
+
+// for(let i=0;i<4;i++){
+//     console.log("---- Value of i "+i);
+// }
+// console.log("Access i after the loop "+i);
+
+// const => to assign const value 
+// scope => global as well as local scope 
+
+const pi=3.14;
+// pi=2.5;
+console.log("Pi value is "+pi);
+
+
+
+
+
+
+
+
