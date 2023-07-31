a435a8627f2fd757b9d93a718109372a6ace265e
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2023-07-31 21:40:58 +0530

    Day09

---
+++ Typescript/Day09/demo2.ts
@@ -0,0 +1,13 @@
+import {Myclass,add,pi} from './demo1';
+
+let obj=new Myclass(3,'Spruha','Raokhande');
+obj.display();
+
+
+console.log(`
+    Addition ${add(10,2)}
+`);
+
+console.log(`
+    PI Is ${pi}
+`)
---
+++ Typescript/Myfolder/demo3.ts
@@ -0,0 +1,5 @@
+import {Myclass} from '../Day09/demo1';
+
+let obj=new Myclass(6,'Kiran','Raokhande');
+
+obj.display();
