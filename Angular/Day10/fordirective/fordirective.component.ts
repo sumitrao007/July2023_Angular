13bbaa2ed0eb206ee99155dde40da1a947b269b6
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2023-08-25 21:27:05 +0530

    Day10

---
+++ Angular/Day10/fordirective/fordirective.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fordirective',
+  templateUrl: './fordirective.component.html',
+  styleUrls: ['./fordirective.component.css']
+})
+export class FordirectiveComponent implements OnInit {
+
+  course:string[]=['Core Java','Spring Boot','HTML.CSS,JS','Angular 12','Docker','AWS','Kubernates','Jenkings','Data Analysis','Machine Learning'];
+  
+  arrobj=[
+      {
+        name:'Samasung',
+        price:25000,
+        qty:1
+      },
+      {
+        name:'iPhone',
+        price:100000,
+        qty:1
+      },
+      {
+        name:'One Plus',
+        price:45000,
+        qty:1
+      },
+      {
+        name:'Motorolla',
+        price:30000,
+        qty:1
+      },
+      {
+        name:'Realme',
+        price:18000,
+        qty:1
+      }
+
+  ];
+
+  selected:string='';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onMouseover(item:any){
+    console.log("u r in onMouseover");
+    // console.log(item)
+    this.selected=item.name;
+  }
+
+  onMouseOut(){
+    this.selected='';
+  }
+
+}
